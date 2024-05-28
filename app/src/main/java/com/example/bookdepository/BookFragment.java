777ac5f346793d5d3b5b132cc2b759ca6c430def
Maxim Volkov <m.volk2606@mail.ru>
2024-05-28 11:05:20 +0300

    9 глава практики

--- app/src/main/java/com/example/bookdepository/BookFragment.java
+++ app/src/main/java/com/example/bookdepository/BookFragment.java
@@ -11,50 +11,65 @@ import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 
+
 import androidx.fragment.app.Fragment;
 
-import java.text.DateFormat;
+import java.util.UUID;
 
 public class BookFragment extends Fragment {
-    private Book mBook;
-    private EditText mTitleField;
-    private Button mDateButton;
-    private CheckBox mReadedCheckBox;
+    public EditText mTitleField;
+    public Book mBook;
+    public Button mDateButton;
+    public CheckBox mReadedCheckBox;
 
+    private static final String ARG_BOOK_ID = "book_id";
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mBook = new Book();
-
+        UUID bookId = (UUID) getArguments().getSerializable(ARG_BOOK_ID);
+        mBook = BookLab.get(getActivity()).getBook(bookId);
+    }
+    public static BookFragment newInstance(UUID bookId)
+    {
+        Bundle args = new Bundle();
+        args.putSerializable(ARG_BOOK_ID, bookId);
+        BookFragment fragment = new BookFragment();
+        fragment.setArguments(args);
+        return fragment;
     }
 
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_book, container, false);
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup
+            container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_book, container,
+                false);
         mTitleField = (EditText) v.findViewById(R.id.book_title);
+        mTitleField.setText(mBook.getTitle());
         mTitleField.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-                // Здесь намеренно оставлено пустое место
+
             }
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mBook.setTitle(s.toString());
             }
-
 
             @Override
             public void afterTextChanged(Editable s) {
-                // И здесь тоже
+
             }
         });
-        mDateButton = (Button) v.findViewById(R.id.book_date);
+        mDateButton = (Button)v.findViewById(R.id.book_date);
         mDateButton.setText(mBook.getDate().toString());
         mDateButton.setEnabled(false);
-        mReadedCheckBox = (CheckBox) v.findViewById(R.id.book_readed);
+        mReadedCheckBox = (CheckBox)v.findViewById(R.id.book_readed);
+        mReadedCheckBox.setChecked(mBook.isReaded());
         mReadedCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                // Назначение флага прочтения книги
+                mBook.setReaded(isChecked);
             }
         });
         return v;
--- app/src/main/java/com/example/bookdepository/BookListFragment.java
+++ app/src/main/java/com/example/bookdepository/BookListFragment.java
@@ -1,12 +1,13 @@
 package com.example.bookdepository;
 
-import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -17,39 +18,46 @@ import java.util.List;
 public class BookListFragment extends Fragment {
     private RecyclerView mBookRecyclerView;
     private BookAdapter mAdapter;
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_book_list, container, false);
-        mBookRecyclerView = (RecyclerView) view
-                .findViewById(R.id.book_recycler_view);
+        mBookRecyclerView = (RecyclerView) view.findViewById(R.id.book_recycler_view);
         mBookRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
         updateUI();
         return view;
     }
-
-    private void updateUI() {
-        BookLab booklab = BookLab.get(getActivity());
-        List<Book> books = booklab.getBooks();
-        mAdapter = new BookAdapter(books, getActivity());
-        mBookRecyclerView.setAdapter(mAdapter);
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        updateUI();
     }
+    private void updateUI() {
+        BookLab bookLab = BookLab.get(getActivity());
+        List<Book> books = bookLab.getBooks();
+        if (mAdapter == null){
 
-    private class BookHolder extends RecyclerView.ViewHolder {
 
+            mAdapter = new BookAdapter(books) ;
+            mBookRecyclerView.setAdapter(mAdapter);
+        }
+        else{
+            mAdapter.notifyDataSetChanged();
+        }
+    }
+    private class BookHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
         private Book mBook;
+
         private TextView mTitleTextView;
         private TextView mDateTextView;
         private CheckBox mReadedCheckBox;
 
         public BookHolder(View itemView) {
             super(itemView);
-            mTitleTextView = (TextView)
-                    itemView.findViewById(R.id.list_item_book_title_text_view);
-            mDateTextView = (TextView)
-                    itemView.findViewById(R.id.list_item_book_date_text_view);
-            mReadedCheckBox = (CheckBox)
-                    itemView.findViewById(R.id.list_item_book_readed_check_box);
+            itemView.setOnClickListener(this);
+            mTitleTextView = (TextView) itemView.findViewById(R.id.list_item_book_title_text_view);
+            mDateTextView = (TextView) itemView.findViewById(R.id.list_item_book_date_text_view);
+            mReadedCheckBox = (CheckBox) itemView.findViewById(R.id.list_item_book_readed_check_box);
         }
         public void bindBook (Book book){
             mBook = book;
@@ -57,33 +65,36 @@ public class BookListFragment extends Fragment {
             mDateTextView.setText(mBook.getDate().toString());
             mReadedCheckBox.setChecked(mBook.isReaded());
         }
+        public void onClick (View v){
+//            Intent intent = MainActivity.newIntent(getActivity(), mBook.getId());
+//            startActivity(intent);
+            Intent intent = BookActivity.newIntent(getActivity(), mBook.getId());
+            startActivity(intent);
+//            Toast.makeText(getActivity(), mBook.getTitle()+" click", Toast.LENGTH_SHORT).show();
+        }
+
     }
-
-    private class BookAdapter extends RecyclerView.Adapter<BookHolder> { // Удалите "abstract", если это конкретный класс
-        private List<Book> mBooks;
-        private Context mContext;
-
-        public BookAdapter(List<Book> books, Context context) {
-            mBooks = books;
-            mContext = context;
+    private class BookAdapter extends RecyclerView.Adapter<BookHolder>{
+        private List<Book> mBoooks;
+        public BookAdapter(List<Book> books){
+            mBoooks = books;
         }
-
         @Override
-        public BookHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            LayoutInflater layoutInflater = LayoutInflater.from(mContext);
+        public BookHolder onCreateViewHolder(ViewGroup parent, int viewType){
+            LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
             View view = layoutInflater.inflate(R.layout.list_item_book, parent, false);
-            return new BookHolder(view);
+            return  new BookHolder(view);
         }
-
         @Override
-        public void onBindViewHolder(BookHolder holder, int position) {
-            Book book = mBooks.get(position);
+        public void onBindViewHolder(BookHolder holder, int position){
+            Book book = mBoooks.get(position);
+
             holder.bindBook(book);
         }
-
         @Override
-        public int getItemCount() {
-            return mBooks.size();
+        public int getItemCount(){
+            return  mBoooks.size();
         }
+
     }
 }
